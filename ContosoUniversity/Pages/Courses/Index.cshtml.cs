998abb7e427927c8ff495dcff13eafaef0450969
Kristiina Ragni <krragn@ttu.ee>
2021-05-20 08:34:59 +0300

    Project

---
+++ ContosoUniversity/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using ContosoUniversity.Models.SchoolViewModel;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Courses { get;set; }
+
+        /*public async Task OnGetAsync()
+        {
+            Courses = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToListAsync();
+        }*/ //Part 6
+
+        public IList<CourseViewModel> CourseVM { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            CourseVM = await _context.Courses
+            .Select(p => new CourseViewModel
+            {
+                CourseID = p.CourseID,
+                Title = p.Title,
+                Credits = p.Credits,
+                DepartmentName = p.Department.Name
+            }).ToListAsync();
+        }
+    }
+}
